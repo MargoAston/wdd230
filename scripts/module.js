ef8e8c84750a683b0050ba4a48dabc4af2ebeecf
margoaston <margo.aston@yahoo.com>
2023-01-06 19:23:12 -0800

    correct current year in footer copyright

--- scripts/module.js
+++ scripts/module.js
@@ -2,3 +2,6 @@
 var lastUpdated = `Last updated: ${document.lastModified}`;
 
 document.getElementById("updateDate").innerText=lastUpdated;
+ 
+var copyrightyear = `© ${new Date().getFullYear()} .:|:. Margo Aston .:|:. California`;
+document.getElementById("copyright").innerText=copyrightyear;
