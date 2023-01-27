6ba6a3a4626388c69f9a000e82d450135efb8780
margoaston <margo.aston@yahoo.com>
2023-01-27 06:51:54 -0800

    adjust layout of heading

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,3 +1,18 @@
+/*This is the date info*/
+const options = {
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+}
+
+const weekday = ["Sunday, ", "Monday, ", "Tuesday, ", "Wednesday, ", "Thursday, ", "Friday, ", "Saturday, "];
+const d = new Date();
+let today = weekday[d.getDay()];
+
+document.getElementById("day").innerHTML = today;
+document.getElementById("date").innerHTML = new Date(). toLocaleDateString("en-US", options)
+
+/*This is the hamberger button info*/
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
@@ -5,3 +20,12 @@ function toggleMenu() {
 
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
+
+/*This is the copyright line */
+const copyrightYear = document.querySelector("#copyright");
+copyrightYear.textContent = `© ${new Date().getFullYear()} .:|:. Margo Aston .:|:. California`;
+
+/*This is the update line*/
+var lastUpdated = `Last modified: ${document.lastModified}`;
+
+document.getElementById("update-date").innerText=lastUpdated;
