7212653f54dc57dac0e9dc18cdb2eabb963aaea2
margoaston <margo.aston@yahoo.com>
2023-02-11 20:08:00 -0800

    added imageObserver

--- lesson7/scripts/lazyload.js
+++ lesson7/scripts/lazyload.js
@@ -1,17 +1,35 @@
 //reference to all images
-let imagesToLoad = document.querySelectorAll("img[data-src]");
+const images = document.querySelectorAll("[data-src]");
 
-//optional parameters being set for the intersectionalObserver
+function preloadImage(img) {
+  const src = img.getAttribute("data-src");
+  if(!src) {
+    return;
+  }
 
-//moves the path from data-src to src
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {
-    image.removeAttribute("data-src");
-  };
+  img.src = src;
+}
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px"
 };
 
-//Loop through each img on checl status and load if necessary
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
+//optional parameters being set for the intersectionalObserver
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  })
+}, imgOptions);
+
+images.forEach(image => {
+  imgObserver.observe(image);
+})
+
+
+
