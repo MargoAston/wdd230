6048bc7c9e6ee1bb1ce385f285b48522edebc136
margoaston <margo.aston@yahoo.com>
2023-03-07 18:55:31 -0800

    open weather test api activity

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,37 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+
+// asynchronous function that will use fetch() to request the given api url and then try to convert the response using a JSON parser that is built-in
+async function apiFetch() {
+    const url = 'http://api.openweathermap.org/data/2.5/weather?q=fairbanks&appid=e069384a0b1b43e6c4d948c86089b178&units=imperial&units=imperial';
+
+    
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong> ${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
