8e3a17cba0726315bfa2743fe125a3be8dfd1712
margoaston <margo.aston@yahoo.com>
2023-03-20 18:06:54 -0700

    nav and hero styles

---
+++ bountiful-foods/scripts/scripts.js
@@ -0,0 +1,7 @@
+/*This is the hamburger button info*/
+function toggleMenu(x) {
+    document.getElementById("primary-nav").classList.toggle("open");
+    document.getElementById("container").classList.toggle("open");
+
+    x.classList.toggle("change");
+}
